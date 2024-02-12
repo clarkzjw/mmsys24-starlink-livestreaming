a68a47bb422b386bf5c44fe68b7cdd698461241c
clarkzjw <i@jinwei.me>
2024-02-12 15:57:55 -0800

    add scripts to convert irtt.json to latency trace

---
+++ runner/trace/convert.py
@@ -0,0 +1,26 @@
+import csv
+import math
+
+
+with open("irtt.csv", "r") as trace:
+    reader = csv.reader(trace)
+    count = 0
+    init_time = 0
+    previous_time = 0
+    with open("trace.csv", "w", newline='') as outcsv: 
+        writer = csv.DictWriter(outcsv, fieldnames = ["since", "relative_seconds", "rtt"])
+        writer.writeheader()
+        
+        for row in reader:
+            if count == 0:
+                count += 1
+                continue
+            if count == 1:
+                previous_time = int(row[0])
+                init_time = int(row[0])
+            count += 1
+            relative = round((int(row[0]) - previous_time) / 1000000000, 3)
+            since = round((int(row[0]) - init_time) / 1000000000, 3)
+            writer.writerows([{"since": since, "relative_seconds": relative, "rtt": row[1]}])
+            previous_time = int(row[0])
+        print("Done")
