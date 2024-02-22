f3f822a545e64759c740ee7d4bc4c0fdd9c64e80
clarkzjw <i@jinwei.me>
2024-02-21 20:39:11 -0800

    fix shaper

--- runner/tc-trace.py
+++ runner/tc-trace.py
@@ -6,12 +6,13 @@ from datetime import datetime
 from pprint import pprint
 
 ETHERNET = "eth0"
-INTERVAL = os.getenv("INTERVAL", 0.1)
+INTERVAL = os.getenv("INTERVAL", 0.01)
 
 
-def set_delay(rtt: float):
+def set_static_delay(rtt: int):
     loss = 1
-    if rtt == -1:
+    if rtt == 0 or rtt == -1:
+        rtt = 100
         loss = 100
     subprocess.check_output(["tc", "qdisc", "change", "dev", ETHERNET, "root", "handle", "1:", "tbf", "rate", "50mbit", "burst", "16kbit", "latency", "{}ms".format(rtt)])
     subprocess.check_output(["tc", "qdisc", "change", "dev", ETHERNET, "parent", "1:1", "handle", "10:", "netem", "delay", "{}ms".format(rtt), "loss", "{}%".format(loss)])
@@ -36,14 +37,14 @@ if __name__ == "__main__":
 
     # read the entire latency trace into memory
     latency_trace = {}
-    with open("./trace/trace.csv", "r") as f:
+    with open("/trace/trace.csv", "r") as f:
         count = 0
         for line in f:
             if count == 0:
                 count += 1
                 continue
             since, relative, rtt = line.strip("\n").split(",")
-            latency_trace[since] = rtt
+            latency_trace[since] = int(float(rtt))
             count += 1
             
     # get init time
@@ -57,9 +58,7 @@ if __name__ == "__main__":
         
         # get the relative time
         relative = now - init
-        # print(relative.total_seconds())
         
-        # find the closest time in the trace
         # get the latency trace with the closest time from the trace
         closest = min(latency_trace, key=lambda x: abs(float(x) - relative.total_seconds()))
         rtt = latency_trace[closest]  
---
+++ runner/trace/plot.py
@@ -0,0 +1,52 @@
+import os
+import re
+from pathlib import Path
+import matplotlib.pyplot as plt
+
+
+def time_series_plot(filename: str, seq_list: list[int], rtt_list: list[float]) -> None:
+    assert(len(seq_list) == len(rtt_list))
+
+    fig = plt.figure(figsize =(21, 7))
+    ax = fig.add_subplot(111)
+
+    ax.plot(seq_list, rtt_list, '.')
+    # ax.set_xlim(0, len(seq_list))
+    plt.title(filename)
+    plt.tight_layout()
+    plt.savefig(str(filename) + ".png")
+    plt.close()
+
+
+def plot(filename: str):
+    if Path(filename).is_file() == False:
+        print("{} doesn't exist".format(filename))
+        exit(1)
+
+    print(filename)
+    with open(filename, "r") as f:
+        count = 0
+        seq_list = []
+        rtt_list = []
+        for line in f.readlines():
+            match = re.search(r"\[(\d+\.\d+)\].*icmp_seq=(\d+).*time=(\d+(\.\d+)?)", line)
+            if match:
+                count += 1
+                timestamp = float(match.group(1))
+                seq = int(match.group(2))
+                rtt = float(match.group(3))
+                seq_list.append(seq)
+                rtt_list.append(rtt)
+
+        assert(len(seq_list) == len(rtt_list))
+        print(len(seq_list))
+        time_series_plot(filename, seq_list, rtt_list)
+
+
+if __name__ == "__main__":
+    path = Path("/home/clarkzjw/test/")
+    for dirpath, dirnames, files in os.walk(path):
+        if len(files) != 0:
+            for f in files:
+                if f.startswith("ping-") and f.endswith(".txt"):
+                    plot(path.joinpath(f))
